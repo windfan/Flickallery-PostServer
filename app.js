c671b3d45353665850b621d2ef3aa457275f5aff
Tzu-Hao Huang <ryu1031@gmail.com>
2016-01-26 01:02:31 -0500

    final step to grab data from Flickr and post them to pub/sub server

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ var keys = {"api_key": key};
 var flickr = new Flickr(keys);
 var urls = require('./controller/urls');
 
-flickr.get("photos.search", {"tags":"cats", "per_page": 10, "page": 1}, function(err, result){
-    if (err) return console.error(err);
-    urls.makeUrl(result.photos.photo, function(error, photoLink){
-      if(error) console.log(error);
-      //urls.postUrl(photoLink);
-    });
-});
+(function _request () {
+  flickr.get("photos.search", {"tags":"cats", "per_page": 1, "page": Math.floor((Math.random() * 1000) + 1)}, function(err, result){
+      if (err) return console.error(err);
+      urls.makeUrl(result.photos.photo, function(error, photoLinks){
+        if(error) console.log(error);
+        urls.postUrl(photoLinks, function(error){
+          if(error) console.log(error);
+        });
+      });
+  });
+  setTimeout(_request, 5000);
+})();
--- controller/urls.js
+++ controller/urls.js
@@ -1,21 +1,40 @@
 'use strict';
 
 var fUrl = require('flickr-urls');
-
+var http = require('http');
+var request = require('request');
+/* make urls based on the photos array
+ * @param: {Array} photos
+ * @param: {Function} callback
+ */
 module.exports.makeUrl = function(photos, callback) {
-  var params = {
-  	server: '',
-  	secret: '',
-  	id: '',
-  	farm: '',
-  	size: fUrl.IMG_SIZES.LARGE_1024
-  };
+  var urlArr = [];
+  var params = { server: '', secret: '', id: '', farm: '', size: fUrl.IMG_SIZES.LARGE_1024 };
   for(var index in photos) {
     params.server = photos[index].server;
     params.secret = photos[index].secret;
     params.id = photos[index].id;
     params.farm = photos[index].farm;
     var url = fUrl.getFarmUrl(params);
-    console.log(url);
+    var obj = {"photo_id": url};
+    urlArr.push(obj);
   }
+  //console.log(urlArr);
+  return callback(null, urlArr);
+}
+
+/* post urls based on the photos array
+ * @param: {Array} photoLinks
+ * @param: {Function} callback
+ */
+module.exports.postUrl = function(photoLinks, callback2) {
+  var requestObj = {
+    json: photoLinks,
+    method: "POST",
+    url: "http://localhost:8000"
+  }
+
+	request(requestObj, function(err){
+		if (err) console.log(err);
+	});
 }
